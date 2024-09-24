b780ab1d92a95c4dc5a7d31104a1494ca093a477
gllopessantos <gustavo.lopes.santos17@escola.pr.gov.br>
2024-09-24 09:14:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Brasil',
+    'Quem descobriu o Brasil?',
+    'Foi Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Jogador',
+    'Qual jogador que marcou gerações no Brasil e no mundo?',
+    'Pelé'
+)
+
+criaCartao(
+    'Programação',
+    'a melhor professora de programação?',
+    'Marcia Moro'
+)
+
+criaCartao(
+    'jogador',
+    'Jogador em que as pessoas mais se inspiram?',
+    'Neymar Junior'
+)
